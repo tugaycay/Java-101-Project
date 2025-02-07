073b1a79853e490d9bd192a4819f366721fdd4de
TUGAY <108936202+tugaycay@users.noreply.github.com>
2025-02-08 01:48:49 +0300

    Add files via upload

---
+++ Ogrenci_Bilgi_Sistemi/Teacher.java
@@ -0,0 +1,15 @@
+package Java101.Ogrenci_Bilgi_Sistemi;
+
+public class Teacher {
+    String name;
+    String mpno;
+    String branch;
+
+    public Teacher(String name, String mpno, String branch) {
+        this.name = name;
+        this.mpno = mpno;
+        this.branch = branch;
+    }
+
+}
+
